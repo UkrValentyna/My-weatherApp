7672bfcb9336d52951e64beb0f1cd6c25ad4832c
UkrVakentyna <133866737+UkrVakentyna@users.noreply.github.com>
2023-07-01 17:56:57 +0100

    Updated city

--- index.js
+++ index.js
@@ -23,12 +23,14 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
+  console.log(response.data.name);
+  let cityElement = document.querySelector("#city");
   let temperatureElement = document.querySelector("#temperature");
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
   let descriptionElement = document.querySelector("#description");
   let dateElement = document.querySelector("#date");
+  cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   windElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
   humidityElement.innerHTML = `${Math.round(response.data.main.humidity)}%`;
@@ -37,6 +39,6 @@ function displayTemperature(response) {
 }
 
 let apiKey = "5b9aaac066641215de6d72f73af7e9b5";
-let city = "London";
+let city = "Odesa";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5b9aaac066641215de6d72f73af7e9b5&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
