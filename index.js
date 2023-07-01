321a1b40344238885465933d06d916edc344b9b4
UkrVakentyna <133866737+UkrVakentyna@users.noreply.github.com>
2023-07-01 16:46:57 +0100

    Added description

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function displayTemperature(response) {
-  console.log(response.data.main.humidity);
+  console.log(response.data.weather[0].description);
   let temperatureElement = document.querySelector("#temperature");
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
+  let descriptionElement = document.querySelector("#description");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   windElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
   humidityElement.innerHTML = `${Math.round(response.data.main.humidity)}%`;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "5b9aaac066641215de6d72f73af7e9b5";
